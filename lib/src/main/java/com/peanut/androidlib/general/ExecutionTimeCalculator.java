e9abe8fcce5fa16b399d17a2c8cd0a9a6f528a74
daophilac <daophilac@gmail.com>
2019-05-01 03:41:30 +0700

    minor change

--- lib/src/main/java/com/peanut/androidlib/general/ExecutionTimeCalculator.java
+++ lib/src/main/java/com/peanut/androidlib/general/ExecutionTimeCalculator.java
@@ -3,24 +3,31 @@ package com.peanut.androidlib.general;
 import java.util.Calendar;
 
 public class ExecutionTimeCalculator {
+    private static final String HAVE_NOT_STARTED_YET = "Tracker has not been started yet.";
     private long beforeMillisecond;
     private long afterMillisecond;
+    private boolean started;
     public void startTracking(){
-        beforeMillisecond = Calendar.getInstance().getTimeInMillis();
+        this.beforeMillisecond = Calendar.getInstance().getTimeInMillis();
+        this.started = true;
     }
     public void stopTracking(){
-        afterMillisecond = Calendar.getInstance().getTimeInMillis();
+        if(!this.started){
+            throw new IllegalStateException(HAVE_NOT_STARTED_YET);
+        }
+        this.started = false;
+        this.afterMillisecond = Calendar.getInstance().getTimeInMillis();
     }
 
     public long getBeforeMillisecond() {
-        return beforeMillisecond;
+        return this.beforeMillisecond;
     }
 
     public long getAfterMillisecond() {
-        return afterMillisecond;
+        return this.afterMillisecond;
     }
 
     public long getExecutionTime(){
-        return afterMillisecond - beforeMillisecond;
+        return this.afterMillisecond - this.beforeMillisecond;
     }
 }
