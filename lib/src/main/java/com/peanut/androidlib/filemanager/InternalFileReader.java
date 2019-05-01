01beda723b1e1ecb883426a8c6b257bdd1159279
daophilac <daophilac@gmail.com>
2019-05-01 20:22:04 +0700

    internalfile

---
+++ lib/src/main/java/com/peanut/androidlib/filemanager/InternalFileReader.java
@@ -0,0 +1,94 @@
+package com.peanut.androidlib.filemanager;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InternalFileReader {
+    private static final String CONTEXT_IS_NULL = "Context cannot be null.";
+    private Context context;
+    private String directoryContainer;
+    private String fileName;
+    private String fullPath;
+    private boolean useBaseDirectory;
+    private File file;
+    private FileInputStream fileInputStream;
+    private InputStreamReader inputStreamReader;
+    private BufferedReader bufferedReader;
+    public InternalFileReader(String fileName, Context context){
+        if(context == null){
+            throw new IllegalArgumentException(CONTEXT_IS_NULL);
+        }
+        this.fileName = fileName;
+        this.context = context;
+        this.fullPath = this.context.getFilesDir() + "/" + this.fileName;
+        this.useBaseDirectory = true;
+    }
+    public InternalFileReader(String directoryContainer, String fileName, Context context){
+        if(context == null){
+            throw new IllegalArgumentException("Activity cannot be null.");
+        }
+        this.fileName = fileName;
+        if(directoryContainer.charAt(0) == '/'){
+            if(directoryContainer.charAt(directoryContainer.length() - 1) == '/'){
+                this.directoryContainer = directoryContainer.substring(1, directoryContainer.length() - 1);
+            }
+            else{
+                this.directoryContainer = directoryContainer.substring(1);
+            }
+        }
+        else{
+            if(directoryContainer.charAt(directoryContainer.length() - 1) == '/'){
+                this.directoryContainer = directoryContainer.substring(0, directoryContainer.length() - 1);
+            }
+            else{
+                this.directoryContainer = directoryContainer;
+            }
+        }
+        this.context = context;
+        this.fullPath = this.context.getFilesDir() + "/" + this.directoryContainer + "/" + this.fileName;
+        this.useBaseDirectory = false;
+    }
+    public String getFullPath(){
+        return this.fullPath;
+    }
+    public String readLine(){
+        String line = "";
+        if(this.file == null){
+            this.file = new File(this.fullPath);
+            if(!this.file.exists()){
+                throw new RuntimeException("File " + this.fileName + " doesn't exist.");
+            }
+            else{
+                try {
+                    this.fileInputStream = new FileInputStream(this.file);
+                    this.inputStreamReader = new InputStreamReader(this.fileInputStream);
+                    this.bufferedReader = new BufferedReader(this.inputStreamReader);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            line = this.bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+    public void close(){
+        try {
+            this.bufferedReader.close();
+            this.inputStreamReader.close();
+            this.fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ lib/src/main/java/com/peanut/androidlib/filemanager/InternalFileWriter.java
@@ -0,0 +1,134 @@
+package com.peanut.androidlib.filemanager;
+
+import android.Manifest;
+import android.content.Context;
+
+import com.peanut.androidlib.permissionmanager.PermissionInquirer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class InternalFileWriter {
+    private static final String CONTEXT_IS_NULL = "Context cannot be null.";
+    private static final String READ_EXTERNAL_STORAGE_PERMISSION_HAS_NOT_BEEN_GRANTED = "Read external storage permission has not been granted.";
+    private Context context;
+    private String directoryContainer;
+    private String fileName;
+    private String fullPath;
+    private boolean useBaseDirectory;
+
+    private PermissionInquirer permissionInquirer;
+    public InternalFileWriter(String fileName, Context context){
+        if(context == null){
+            throw new IllegalArgumentException(CONTEXT_IS_NULL);
+        }
+        this.permissionInquirer = new PermissionInquirer(this.context);
+        if(!this.permissionInquirer.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            throw new IllegalStateException(READ_EXTERNAL_STORAGE_PERMISSION_HAS_NOT_BEEN_GRANTED);
+        }
+        this.fileName = fileName;
+        this.context = context;
+        this.fullPath = this.context.getFilesDir().getAbsolutePath() + "/" + this.fileName;
+        this.useBaseDirectory = true;
+    }
+    public InternalFileWriter(String directoryContainer, String fileName, Context context){
+        if(context == null){
+            throw new IllegalArgumentException(CONTEXT_IS_NULL);
+        }
+        this.permissionInquirer = new PermissionInquirer(this.context);
+        if(!this.permissionInquirer.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            throw new IllegalStateException(READ_EXTERNAL_STORAGE_PERMISSION_HAS_NOT_BEEN_GRANTED);
+        }
+        this.fileName = fileName;
+        if(directoryContainer.charAt(0) == '/'){
+            if(directoryContainer.charAt(directoryContainer.length() - 1) == '/'){
+                this.directoryContainer = directoryContainer.substring(1, directoryContainer.length() - 1);
+            }
+            else{
+                this.directoryContainer = directoryContainer.substring(1);
+            }
+        }
+        else{
+            if(directoryContainer.charAt(directoryContainer.length() - 1) == '/'){
+                this.directoryContainer = directoryContainer.substring(0, directoryContainer.length() - 1);
+            }
+            else{
+                this.directoryContainer = directoryContainer;
+            }
+        }
+        this.context = context;
+        this.fullPath = this.context.getFilesDir() + "/" + this.directoryContainer + "/" + this.fileName;
+        this.useBaseDirectory = false;
+    }
+    public String getFullPath(){
+        return this.fullPath;
+    }
+    public void write(String content, boolean append){
+        if(!this.useBaseDirectory){
+            File directory = new File(this.context.getFilesDir() + "/" + this.directoryContainer);
+            if(!directory.exists()){
+                directory.mkdirs();
+            }
+        }
+        File file = new File(this.fullPath);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file, append);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            outputStreamWriter.append(content);
+            outputStreamWriter.close();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void write(String[] contents, boolean append){
+        if(!this.useBaseDirectory){
+            File directory = new File(this.context.getFilesDir() + "/" + this.directoryContainer);
+            if(!directory.exists()){
+                directory.mkdirs();
+            }
+        }
+        File file = new File(this.fullPath);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file, append);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            for (String content : contents) {
+                outputStreamWriter.append(content);
+            }
+            outputStreamWriter.close();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void write(String[] contents, char separator, boolean append){
+        if(!this.useBaseDirectory){
+            File directory = new File(this.context.getFilesDir() + "/" + this.directoryContainer);
+            if(!directory.exists()){
+                directory.mkdirs();
+            }
+        }
+        File file = new File(this.fullPath);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file, append);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            for (String content : contents) {
+                outputStreamWriter.append(content);
+                outputStreamWriter.append(separator);
+            }
+            outputStreamWriter.close();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
