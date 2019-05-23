e3b001d42e8da2a58bd8d6c742dfc03e274496f5
daophilac <daophilac@gmail.com>
2019-05-24 03:37:34 +0700

    Update DistancePicker.java

--- distancepicker/src/main/java/com/peanut/androidlib/view/DistancePicker.java
+++ distancepicker/src/main/java/com/peanut/androidlib/view/DistancePicker.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 
 public class DistancePicker extends LinearLayout {
     private MeasurementPicker measurementPicker;
@@ -38,9 +39,92 @@ public class DistancePicker extends LinearLayout {
         measurementPicker = view.findViewById(R.id.measurement_picker);
         integerPicker = view.findViewById(R.id.multiply_number_picker);
     }
+    public void perform(){
+        integerPicker.perform();
+    }
+    public void setUnitStyle(MeasurementPicker.UnitStyle unitStyle) {
+        measurementPicker.setUnitStyle(unitStyle);
+    }
+    public void setSelectedUnitIndex(int selectedIndex){
+        measurementPicker.setSelectedIndex(selectedIndex);
+    }
+
+    public void setMeasurements(MeasurementPicker.Measurement[] measurements) {
+        measurementPicker.setMeasurements(measurements);
+    }
+
+    public void setBaseMinValue(int baseMinValue) {
+        integerPicker.setBaseMinValue(baseMinValue);
+    }
+
+    public void setBaseMaxValue(int baseMaxValue) {
+        integerPicker.setBaseMaxValue(baseMaxValue);
+    }
+
+    public void setMultiplicationFactor(int multiplicationFactor) {
+        integerPicker.setMultiplicationFactor(multiplicationFactor);
+    }
+
+    public void setSelectedValueIndex(int selectedIndex) {
+        integerPicker.setSelectedIndex(selectedIndex);
+    }
+
+    public void setOnMeasurementChangeListener(MeasurementPicker.OnMeasurementChangeListener onMeasurementChangeListener) {
+        measurementPicker.setOnMeasurementChangeListener(onMeasurementChangeListener);
+    }
+
+    public void setOnValueChangeListener(NumberPicker.OnValueChangeListener onValueChangeListener) {
+        integerPicker.setOnValueChangeListener(onValueChangeListener);
+    }
     public void setWrapSelectorWheel(boolean wrapSelectorWheel){
         measurementPicker.setWrapSelectorWheel(wrapSelectorWheel);
         integerPicker.setWrapSelectorWheel(wrapSelectorWheel);
+    }
+    public MeasurementPicker.UnitStyle getUnitStyle() {
+        return measurementPicker.getUnitStyle();
+    }
+
+    public MeasurementPicker.Measurement getMeasurement() {
+        return measurementPicker.getMeasurement();
+    }
+
+    public MeasurementPicker.Measurement[] getMeasurements() {
+        return measurementPicker.getMeasurements();
+    }
+
+    public String[] getShortMeasurements() {
+        return measurementPicker.getShortMeasurements();
+    }
+
+    public String[] getLongMeasurements() {
+        return measurementPicker.getLongMeasurements();
+    }
+    public int getBaseMinValue() {
+        return integerPicker.getBaseMinValue();
+    }
+
+    public int getBaseMaxValue() {
+        return integerPicker.getBaseMaxValue();
+    }
+
+    public int getMultiplicationFactor() {
+        return integerPicker.getMultiplicationFactor();
+    }
+
+    public int getSelectedIndex() {
+        return integerPicker.getSelectedIndex();
+    }
+
+    public int getNumElement() {
+        return integerPicker.getNumElement();
+    }
+
+    public int[] getValues() {
+        return integerPicker.getValues();
+    }
+
+    public String[] getDisplayValues() {
+        return integerPicker.getDisplayValues();
     }
 
     public MeasurementPicker getMeasurementPicker() {
